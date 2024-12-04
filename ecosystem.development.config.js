96fb379e460568770cf33cf7d6fdd15f0020ff06
orlandoneto23 <106163916+orlandoneto23@users.noreply.github.com>
2024-12-04 12:02:23 -0300

    feat: More improving config file deploy

--- ecosystem.development.config.js
+++ ecosystem.development.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "designflix-api", // Nome da sua aplicação
+      name: "designflix-api-dev", // Nome da sua aplicação
       script: "/root/api/dev/designflix-api/current/src/main.js", // Arquivo principal da aplicação
       instances: 1, // Garantindo que apenas uma instância será executada
       exec_mode: "fork", // Modo de execução "fork" (não cluster)
@@ -11,8 +11,8 @@ module.exports = {
         DOTENV_CONFIG_PATH: "/root/api/dev/designflix-api/current/.env",
         PORT: 3000, // Porta da aplicação em desenvolvimento
       },
-      error_file: "/root/api/prd/designflix-api/shared/logs/error.log",
-      out_file: "/root/api/prd/designflix-api/shared/logs/out.log",     
+      error_file: "/root/api/dev/designflix-api/shared/logs/error.log",
+      out_file: "/root/api/dev/designflix-api/shared/logs/out.log",     
       log_date_format: "YYYY-MM-DD HH:mm Z",
       merge_logs: true, 
     },
@@ -26,7 +26,7 @@ module.exports = {
       repo: "git@github.com:orlandoneto/designflix-api.git", // Repositório Git
       path: "/root/api/dev/designflix-api", // Caminho onde o projeto será implantado
       "post-deploy":
-        "npm install && pm2 reload ecosystem.development.config.js --env development --name designflix-api-prd", // Comando pós-deploy com o nome adicionado
+        "npm install && pm2 reload ecosystem.development.config.js --env development --name designflix-api-dev", // Comando pós-deploy com o nome adicionado
       "pre-deploy-local": "echo 'Preparando deploy of development...'", // Opcional
     },
   },
