d39879f1703801e47308b91e3c3b4df2234127b5
orlandoneto23 <106163916+orlandoneto23@users.noreply.github.com>
2024-12-05 15:43:50 -0300

    feat: More improving config file deploy

--- ecosystem.development.config.js
+++ ecosystem.development.config.js
@@ -26,7 +26,7 @@ module.exports = {
       repo: "git@github.com:orlandoneto/designflix-api.git", // Repositório Git
       path: "/root/api/dev/designflix-api", // Caminho onde o projeto será implantado
       "post-deploy":
-        "npm i && npm run build-dev && ln -sf /root/api/dev/designflix-api/source/src/middleware/private.key /root/api/dev/designflix-api/source/dist/middleware/private.key && pm2 reload ecosystem.development.config.js --env development && pm2 save --force && pm2 list",
+        "npm i && npm run build-stg && ln -sf /root/api/dev/designflix-api/source/src/middleware/private.key /root/api/dev/designflix-api/source/dist/middleware/private.key && pm2 reload ecosystem.development.config.js --env development && pm2 save --force && pm2 list",
       "pre-deploy-local": "echo 'Preparando deploy of development...'", // Opcional
     },
   },
