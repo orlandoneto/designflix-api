6ab41c6320a531198d193355a73804b19d6282c3
orlandoneto23 <106163916+orlandoneto23@users.noreply.github.com>
2024-12-23 14:49:56 -0300

    fix: fixed bug of dist

--- ecosystem.development.config.js
+++ ecosystem.development.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: "designflix-api-dev", // Nome da sua aplicação
-      script: "/root/api/dev/designflix-api/current/src/main.js", // Arquivo principal da aplicação
+      script: "/root/api/dev/designflix-api/source/src/main.js", // Arquivo principal da aplicação
       instances: 1, // Garantindo que apenas uma instância será executada
       exec_mode: "fork", // Modo de execução "fork" (não cluster)
       watch: false, // Desabilita o watch em produção para evitar reinicializações desnecessárias
       env: {
         NODE_ENV: "development", // Ambiente de desenvolvimento
-        DOTENV_CONFIG_PATH: "/root/api/dev/designflix-api/current/.env",
+        DOTENV_CONFIG_PATH: "/root/api/dev/designflix-api/source/.env",
         PORT: 3000, // Porta da aplicação em desenvolvimento
       },
       error_file: "/root/api/dev/designflix-api/shared/logs/error.log",
@@ -24,9 +24,9 @@ module.exports = {
       host: "46.202.146.92", // IP ou Host do servidor
       ref: "origin/main", // Branch do repositório Git que será usada
       repo: "git@github.com:orlandoneto/designflix-api.git", // Repositório Git
-      path: "/root/api/dev/designflix-api", // Caminho onde o projeto será implantado
+      path: "/root/api/dev/designflix-api/source", // Caminho onde o projeto será implantado
       "post-deploy":
-        "npm i && npx sequelize db:seed:undo:all --env development && npx sequelize db:seed:all --env development && pm2 reload ecosystem.development.config.js && pm2 save --force && pm2 list",
+        "npm i && npx sequelize db:seed:undo:all --env development && npx sequelize db:seed:all --env development && pm2 reload ecosystem.config.js --only designflix-api-dev && pm2 save --force && pm2 list",
       "pre-deploy-local": "echo 'Preparando deploy of development...'", // Opcional
     },
   },
--- ecosystem.production.config.js
+++ ecosystem.production.config.js
@@ -2,19 +2,19 @@ module.exports = {
   apps: [
     {
       name: "designflix-api-prd", // Nome da sua aplicação
-      script: "/root/api/prd/designflix-api/current/src/main.js", // Arquivo principal da aplicação
+      script: "/root/api/prd/designflix-api/source/src/main.js", // Arquivo principal da aplicação
       instances: 1, // Garantindo que apenas uma instância será executada
       exec_mode: "fork", // Modo de execução "fork" (não cluster)
       watch: false, // Desabilita o watch em produção para evitar reinicializações desnecessárias
       env: {
         NODE_ENV: "production", // Ambiente de produção
-        DOTENV_CONFIG_PATH: "/root/api/prd/designflix-api/current/.env",
+        DOTENV_CONFIG_PATH: "/root/api/prd/designflix-api/source/.env",
         PORT: 4000, // Porta da aplicação em produção
       },
       error_file: "/root/api/prd/designflix-api/shared/logs/error.log",
-      out_file: "/root/api/prd/designflix-api/shared/logs/out.log",     
+      out_file: "/root/api/prd/designflix-api/shared/logs/out.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      merge_logs: true, 
+      merge_logs: true,
     },
   ],
 
@@ -24,9 +24,9 @@ module.exports = {
       host: "46.202.146.92", // IP ou Host do servidor
       ref: "origin/main", // Branch do repositório Git que será usada
       repo: "git@github.com:orlandoneto/designflix-api.git", // Repositório Git
-      path: "/root/api/prd/designflix-api", // Caminho onde o projeto será implantado
+      path: "/root/api/prd/designflix-api/source", // Caminho onde o projeto será implantado
       "post-deploy":
-      "npm i && npx sequelize db:seed:undo:all --env production && npx sequelize db:seed:all --env production && pm2 reload ecosystem.production.config.js --env production && pm2 save --force && pm2 list",
+        "npm install --production && npx sequelize db:seed:undo:all --env production && npx sequelize db:seed:all --env production && pm2 reload ecosystem.production.config.js --env production && pm2 save --force && pm2 list",
       "pre-deploy-local": "echo 'Preparando deploy of production...'", // Opcional
     },
   },
