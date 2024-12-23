06750e4a02c3d3c0e6d1a40401c70a3bac104996
orlandoneto23 <106163916+orlandoneto23@users.noreply.github.com>
2024-12-23 11:39:04 -0300

    fix: cors to app web

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(helmet());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(express.urlencoded({ extended: true }));
