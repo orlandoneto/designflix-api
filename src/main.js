911d4af7d81c8e2c91e90a24db14476eb8b83d70
orlandoneto23 <106163916+orlandoneto23@users.noreply.github.com>
2024-12-30 10:43:34 -0300

    fix: fixed code otp

--- src/main.js
+++ src/main.js
@@ -66,9 +66,7 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-if (process.env.API_URL.length > 0) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-}
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/favicon.ico", (req, res) => {
   res.sendStatus(204);
